e288aab294e6e94aea0839825c714ca99f72cb37
DESKTOP-QIBNG2D\pkjun <pkjuno@nate.com>
2024-08-20 18:07:58 +0900

    최초 커밋

---
+++ src/main/java/com/project/Heritage/projectHeritage/ProjectHeritageApplication.java
@@ -0,0 +1,13 @@
+package com.project.Heritage.projectHeritage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHeritageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHeritageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/Heritage/projectHeritage/ProjectHeritageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Heritage.projectHeritage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHeritageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
